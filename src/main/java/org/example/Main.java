17dde853934ae5b88448e7a134ae66e2d0a5447c
Baronas0927 <do.baublys@gmail.com>
2024-10-11 15:01:26 +0300

    atnaujinimas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,18 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Sveiki atvykę į Lietuvos Geležinkelių ir banko programą");
+        System.out.println("Pasirinkite norimą programą:");
+        Scanner scanner = new Scanner(System.in);
+        try{
+            switch ()
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
+
 }
