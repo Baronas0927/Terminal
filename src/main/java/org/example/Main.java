9eb7ffd4ee23fab7fe4f1a65fdeee4be4e3e0538
Baronas0927 <do.baublys@gmail.com>
2024-10-11 15:16:34 +0300

    atnaujinimas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,17 +2,37 @@ package org.example;
 
 import java.util.Scanner;
 
+import static java.lang.System.exit;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Sveiki atvykę į Lietuvos Geležinkelių ir banko programą");
         System.out.println("Pasirinkite norimą programą:");
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("1. Lietuvos Gelezinkeliai");
+        System.out.println("2. Bankas");
+        System.out.println("3. Išeiti iš programos");
         try{
-            switch ()
+            Scanner sc = new Scanner(System.in);
+            int i = sc.nextInt();
+            switch (i) {
+                case 1:
+                    LTG();
+                case 2:
+                    Bankas();
+                case 3:
+                    exit(0);
+            }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static void Bankas() {
+    }
+
+    private static void LTG(){
+        
     }
 
 }
